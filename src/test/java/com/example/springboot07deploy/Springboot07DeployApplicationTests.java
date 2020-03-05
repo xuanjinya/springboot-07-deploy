1ce553e9b0e8f74c9a426de70f301330d126f93f
xuanjinya <1067998193@qq.com>
2020-03-05 11:59:47 +0800

    自动热部署，pom.xml引入devtools，CTRL+F9 进行 Build

---
+++ src/test/java/com/example/springboot07deploy/Springboot07DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot07deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07DeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
